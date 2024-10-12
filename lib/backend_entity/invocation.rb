8f0e1bc0b3aea3f532ce32ec27ca49aac123d548
Michael Ajwani <michael.ajwani@i22.de>
2024-10-12 23:24:41 +0200

    wip | Issue #3

---
+++ lib/backend_entity/invocation.rb
@@ -0,0 +1,45 @@
+module BackendEntity
+  module Invocation
+    extend ActiveSupport::Concern
+
+    included do
+      class_attribute :entity_model_name, :current_entity_name, :current_entity_class
+      # class_attribute :entity_name, :entity_model, :entity_class
+    end
+
+    module ClassMethods do
+
+    end
+
+    # class << self
+    #   def included(base)
+    #     base.extend(ClassMethods)
+    #   end
+    # end
+
+    # module ClassMethods
+    #   def access(*args)
+    #     options = args.extract_options!
+    #     options[:only] = args
+    #     options[:except] = [] if options[:only].present?
+    #     options[:except] = args if options[:only].blank?
+
+    #     before_action(options) do
+    #       access = Access.new(self)
+    #       access.check_permissions
+    #     end
+    #   end
+    # end
+
+    # def initialize(controller)
+    #   @controller = controller
+    # end
+
+    # def check_permissions
+    #   return if @controller.current_user&.admin?
+
+    #   if @controller.action_name.in?(options[:except])
+    #     raise BackendEntity::GenericError, 'You are not allowed to access this resource.'
+    #   end
+    # end
+  end
---
+++ test/lib/backend_entity/invocation_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module BackendEntity
+  class InvocationTest < ActiveSupport::TestCase
+  end
+end
