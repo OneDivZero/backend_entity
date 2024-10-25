8197c2612d4048ddca0e5354cf5eeedd684a1c35
Michael Ajwani <michael.ajwani@i22.de>
2024-10-25 16:39:12 +0200

    improved test-reporting | Issue #3

---
+++ test/support/config/reporting.rb
@@ -0,0 +1,17 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Test-Reporting-Config
+#-----------------------------------------------------------------------------------------------------------------------
+
+test_reporters = [Minitest::Reporters::DefaultReporter.new(colors: true)] # Gitlab-CI
+
+if RUBY_PLATFORM.match?(/darwin/) # OSX-Only
+  require 'minitest/autorun'
+  require 'minitest/macos_notification'
+
+  test_reporters.push(Minitest::Reporters::SpecReporter.new)
+  test_reporters.push(Minitest::Reporters::MacosNotificationReporter.new(title: 'BackendEntity'))
+end
+
+if ENV.fetch('USE_MINITEST_REPORTERS', 'true') == 'true'
+  Minitest::Reporters.use!(test_reporters, ENV, Minitest.backtrace_filter)
+end
