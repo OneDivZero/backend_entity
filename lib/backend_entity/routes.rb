6bf88b9bca890f8bebdc2be5e7a511f6d8188720
Michael Ajwani <michael.ajwani@i22.de>
2024-10-28 13:05:12 +0100

    simplified concern | Issue #3

--- lib/backend_entity/routes.rb
+++ lib/backend_entity/routes.rb
@@ -5,48 +5,57 @@ module BackendEntity
     PREFIX = 'backend'.freeze
 
     included do
+      alias_method :entity_path, :entity_show_path
+      alias_method :entity_path_name, :entity_show_path_name
     end
 
     def entity_index_path(params: {})
       public_send(entity_index_path_name, params)
     end
 
-    def entity_show_path(entity = nil, params: {})
-      entity ||= @entity
-      public_send(entity_show_path_name, entity, params)
+    %w[show create update destroy new edit].each do |action_name|
+      define_method("entity_#{action_name}_path") do |entity = nil, params: {}|
+        entity ||= @entity
+        public_send("entity_#{action_name}_path_name".to_sym, entity, params)
+      end
     end
 
-    def entity_create_path(entity = nil, params: {})
-      entity ||= @entity
-      public_send(entity_create_path_name, entity, params)
+    # def entity_show_path(entity = nil, params: {})
+    #   entity ||= @entity
+    #   public_send(entity_show_path_name, entity, params)
+    # end
+
+    # def entity_create_path(entity = nil, params: {})
+    #   entity ||= @entity
+    #   public_send(entity_create_path_name, entity, params)
+    # end
+
+    # def entity_update_path(entity = nil, params: {})
+    #   entity ||= @entity
+    #   public_send(entity_update_path_name, entity, params)
+    # end
+
+    # def entity_destroy_path(entity = nil, params: {})
+    #   entity ||= @entity
+    #   public_send(entity_destroy_path_name, entity, params)
+    # end
+
+    # def entity_new_path(entity = nil, params: {})
+    #   entity ||= @entity
+    #   public_send(entity_new_path_name, entity, params)
+    # end
+
+    # def entity_edit_path(entity = nil, params: {})
+    #   entity ||= @entity
+    #   public_send(entity_edit_path_name, entity, params)
+    # end
+
+    def entity_list_path(params: {})
+      public_send(entity_list_path_name, params)
     end
 
-    def entity_update_path(entity = nil, params: {})
-      entity ||= @entity
-      public_send(entity_update_path_name, entity, params)
-    end
-
-    def entity_destroy_path(entity = nil, params: {})
-      entity ||= @entity
-      public_send(entity_destroy_path_name, entity, params)
-    end
-
-    def entity_new_path(entity = nil, params: {})
-      entity ||= @entity
-      public_send(entity_new_path_name, entity, params)
-    end
-
-    def entity_edit_path(entity = nil, params: {})
-      entity ||= @entity
-      public_send(entity_edit_path_name, entity, params)
-    end
-
-    def entity_list_path
-      public_send(entity_list_path_name)
-    end
-
-    def entity_search_path
-      public_send(entity_search_path_name)
+    def entity_search_path(params: {})
+      public_send(entity_search_path_name, params)
     end
 
     protected def entity_route_key
@@ -62,15 +71,15 @@ module BackendEntity
     end
 
     private def entity_create_path_name
-      entity_show_path_name
+      entity_path_name
     end
 
     private def entity_update_path_name
-      entity_show_path_name
+      entity_path_name
     end
 
     private def entity_destroy_path_name
-      entity_show_path_name
+      entity_path_name
     end
 
     private def entity_new_path_name
