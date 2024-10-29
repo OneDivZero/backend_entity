fdb8b5e96e2c9ce27aa4e66795b04791cda05793
Michael Ajwani <michael.ajwani@i22.de>
2024-10-30 00:17:55 +0100

    improved concern Flashes | Issue #4

--- lib/backend_entity/flashes.rb
+++ lib/backend_entity/flashes.rb
@@ -1,18 +1,21 @@
 # TODO: This a fast integration of the flash-messages, which are used in the actions concern and needs to be reworked #4
-# We replace here now the usage of a decorator as it was in the previous implementation! Thus alsi I18n is required!
+# We replace here now the usage of a decorator as it was in the previous implementation! Thus also I18n is required!
 module BackendEntity
   module Flashes
     extend ActiveSupport::Concern
 
+    # TODO: I18N is missing #4
     CRUD_WORDINGS = {
       create: 'erstellt',
       update: 'geändert',
       destroy: 'gelöscht'
     }.freeze
 
+    # NOTE: In the former implementation the keyword-arg :wording was present, we skipped it now! #4
+    # Now we rely fully on 'model_name.human' instead ... (usage of I18n should be favoured)
     def entity_flash_message_for(action, on: , result: false, error: nil)
       if result
-        flash[:notice] = "#{entity_wording_for(on)} wurde erfolgreich #{action_wording_for(action)}."
+        flash[:notice] = notice_flash_for(action, on)
       else
         cause = base_error_cause_for(on)
         msg = error.nil? ? "#{entity_wording_for(on)} konnte nicht #{action_wording_for(action)} werden." : error.to_s
@@ -34,6 +37,10 @@ module BackendEntity
 
     private def base_error_cause_for(entity)
       entity.errors[:base]&.first if entity.respond_to?(:errors) && entity.errors[:base].present?
+    end
+
+    private def notice_flash_for(action, entity)
+      "#{entity_wording_for(entity)} wurde erfolgreich #{action_wording_for(action)}."
     end
   end
 end
